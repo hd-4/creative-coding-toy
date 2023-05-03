3e6971beddc4c0d9623f0c7a17fefc4bee2df3ef
daniel <131055090+hd-4@users.noreply.github.com>
2023-05-02 21:55:12 -0400

    fix generate window names

--- packages/creative-coding-toy/scripts/generate_window_names.js
+++ packages/creative-coding-toy/scripts/generate_window_names.js
@@ -52,7 +52,7 @@ ts.forEachChild(input_file, (node) => {
 members.sort();
 
 const output_path = fileURLToPath(
-	new URL("../src/vite/window_names.js", import.meta.url)
+	new URL("../src/p5_transform/window_names.js", import.meta.url)
 );
 fs.writeFileSync(
 	output_path,
