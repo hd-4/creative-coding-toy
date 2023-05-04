cff412019d74069918e8224e004a254da24d771b
daniel <131055090+hd-4@users.noreply.github.com>
2023-05-04 18:07:43 -0400

    move runtime folder

--- packages/creative-coding-toy/src/vite/utils.js
+++ packages/creative-coding-toy/src/vite/utils.js
@@ -2,7 +2,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 export const runtime_directory = fileURLToPath(
-	new URL("../runtime", import.meta.url)
+	new URL("./runtime", import.meta.url)
 );
 
 export const runtime_base = runtime_directory.startsWith(
