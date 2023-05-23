54b030fbbd0addd0b460a6a96a6da809b72a2413
daniel <131055090+hd-4@users.noreply.github.com>
2023-05-22 23:02:55 -0400

    inital svelte tests

---
+++ packages/creative-coding-toy/src/vite/test/basics/projects/svelte-basic/+project.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+	export const input_config = {
+		value_a: "foo"
+	};
+</script>
+
+<script>
+	export let inputs;
+</script>
+
+<div data-testid="output">{inputs.value_a}</div>
