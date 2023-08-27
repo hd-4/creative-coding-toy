7842360cd4ce88cedbd18085a85001bf31b6e0f8
daniel <131055090+hd-4@users.noreply.github.com>
2023-08-26 23:18:02 -0400

    Fix p5 HMR after exceptions

--- packages/creative-coding-toy/src/vite/runtime/engine_p5.js
+++ packages/creative-coding-toy/src/vite/runtime/engine_p5.js
@@ -18,15 +18,28 @@ export function mount(mod, element, options = {}) {
 	 * @param {any} sketch
 	 */
 	function sketch_wrapper(sketch) {
+		p5_instance = sketch;
+
 		mod.default(sketch);
 		sketch.preload = p5_method_wrapper(inputs, "preload", sketch.preload);
 		sketch.setup = p5_method_wrapper(inputs, "setup", sketch.setup);
 		sketch.draw = p5_method_wrapper(inputs, "draw", sketch.draw);
 		sketch.inputs = inputs.proxy;
-		p5_instance = sketch;
 	}
 
-	let sketch_instance = new p5(sketch_wrapper, element);
+	// p5's constructor calls the setup() and draw() methods before returning,
+	// which means an exception will stop it from returning an instance.
+	// Instead, capture the instance in the sketch wrapper and call the
+	// constructor without an assignment.
+	function mount_and_catch_errors() {
+		try {
+			new p5(sketch_wrapper, element);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+
+	mount_and_catch_errors();
 
 	return {
 		/**
@@ -35,15 +48,15 @@ export function mount(mod, element, options = {}) {
 		update_inputs(new_inputs) {
 			const affected_spans = inputs.update(new_inputs);
 			if (affected_spans.has("preload") || affected_spans.has("setup")) {
-				sketch_instance.remove();
-				sketch_instance = new p5(sketch_wrapper, element);
+				p5_instance.remove();
+				mount_and_catch_errors();
 			} else if (affected_spans.has("draw")) {
 				p5_instance.redraw();
 			}
 		},
 
 		destroy() {
-			sketch_instance.remove();
+			p5_instance.remove();
 		}
 	};
 }
